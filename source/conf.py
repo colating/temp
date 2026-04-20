32e24d1fabd2c6223ad076fbeda55734ca63866f
1 <1@1.1>
2026-04-20 16:16:48 +0800

    x22mm

--- source/conf.py
+++ source/conf.py
@@ -16,8 +16,8 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme',
-]
+## 'sphinx_rtd_theme',
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -27,7 +27,7 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-##html_theme = 'alabaster'
+html_theme = 'alabaster'
 
-html_theme = "sphinx_rtd_theme"
+##html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
